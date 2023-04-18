978c6977345cc6d525e64a754553b306859e7617
SouravKAgarwal <aggarwalsaurav948@gmail.com>
2023-04-19 02:50:56 +0530

    Completed projects-2

--- pages/projects.js
+++ pages/projects.js
@@ -9,28 +9,32 @@ const Projects = () => {
     <section className='container mx-auto py-10 px-4'>
         <Heading title="My Projects" />
         <div className='grid grid-cols-1 md:grid-cols-3 gap-6 '>
-            <div className='shadow-lg flex flex-col rounded-[10px] gap-4 px-8 py-10 bg-[#64748b] text-[#f4f4f5]'>
+            <div className='shadow-lg flex flex-col rounded-[10px] justify-center align-center gap-4 px-8 py-10 bg-[#64748b] text-[#f4f4f5]'>
                 <GrProjects className='text-[#ef4444] text-[28px]' />
                 <h2 className='font-medium text-[28px]'>Tic-Tac-Toe</h2>
-                <p className='text-[16px] max-height-[168px]'>It is a game made for entertainment purposes. It is a two player game in which one player chooses cross and the other chooses tick and fills the boxes until one of them have three cross or tick in one line.</p>
+                <div className='max-height-[170px]'>
+                <p className='text-[16px]'>It is a game made for entertainment purposes. It is a two player game in which one player chooses cross and the other chooses tick and fills the boxes until one of them have three cross or tick in one line.</p>
+                </div>
                 <div className='mt-4'>
                     <Image className='rounded-[8px] justify-center align-center'width={500} height={250} src="/tictactoe.jpg" alt=""/>
                 </div>
                 <Button link ="https://tictactoe-ska.vercel.app/" text="Click to view" target="_blank" />
             </div>
-            <div className='shadow-lg flex flex-col rounded-[10px] gap-4 px-8 py-10 bg-[#64748b] text-[#f4f4f5]'>
+            <div className='shadow-lg flex flex-col rounded-[10px] justify-center align-center gap-4 px-8 py-10 bg-[#64748b] text-[#f4f4f5]'>
                 <GrProjects className='text-[#ef4444] text-[28px]' />
                 <h2 className='font-medium text-[28px]'>QR-Generator</h2>
-                <p className='text-[16px] max-height-[168px]'>It is a utility which can be used to generate QR's for links,text,etc for handy sharing. It is very reliable, fast and easy to use. It can generate QR's for any text or URL's at a click.</p>
+                <div className='max-height-[170px] mb-4'>
+                    <p className='text-[16px]'>It is a utility which can be used to generate QR's for links,text,etc for handy sharing. It is very reliable, fast and easy to use. It can generate QR's for any text or URL's at a click.</p>
+                </div>
                 <div className='mt-4'>
-                    <Image className=' justify-center align-center rounded-[8px]' src="/qr.jpg" width={500} height={250} alt=""/>
+                    <Image className='rounded-[8px]' src="/qr.jpg" width={500} height={250} alt=""/>
                 </div>
                 <Button link ="https://qr-sourav.vercel.app/" text="Click to view" />
             </div>
-            <div className='shadow-lg flex flex-col gap-4 rounded-[10px] px-8 py-10 bg-[#64748b] text-[#f4f4f5]'>
+            <div className='shadow-lg flex flex-col gap-4 rounded-[10px] justify-center align-center px-8 py-10 bg-[#64748b] text-[#f4f4f5]'>
                 <GrProjects className='text-[#ef4444] text-[28px]' />
                 <h2 className='font-medium text-[28px]'>QR-Generator</h2>
-                <p className='text-[16px] max-height-[168px]'>It is a utility which can be used to generate QR's for links,text,etc for handy sharing. It is very reliable, fast and easy to use. It can generate QR's for any text or URL's at a click.</p>
+                <p className='text-[16px] max-height-[170px] mb-4'>It is a utility which can be used to generate QR's for links,text,etc for handy sharing. It is very reliable, fast and easy to use. It can generate QR's for any text or URL's at a click.</p>
                 <div className='mt-4'>
                     <Image className=' justify-center align-center rounded-[8px]' src="/qr.jpg" width={500} height={250} alt=""/>
                 </div>
