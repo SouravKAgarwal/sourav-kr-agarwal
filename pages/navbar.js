702cc0bfc0acd32ed0f12efbc863b60ddf6bc520
SouravKAgarwal <aggarwalsaurav948@gmail.com>
2023-04-26 20:27:50 +0530

    navbar minor fix

--- pages/navbar.js
+++ pages/navbar.js
@@ -6,7 +6,7 @@ import {HiOutlineDownload} from 'react-icons/hi'
 const Navbar = ({toggleSideBar}) => {
   return (
     <div className='bg-black fixed top-0 w-[100%] z-10'>
-      <div className='container flex items-center py-4'>
+      <div className='container items-center py-4'>
         <div className='text-[24px] text-[#f8fafc] px-4 mx-0'>Sourav Kumar Agarwal</div>
           <div className='hidden md:mx-[10rem] md:justify-around md:items-center md:flex gap-10'>
             <Link href="#">
